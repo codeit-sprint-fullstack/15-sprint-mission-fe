a8f197b46e9ddaf3323379fd78f1f5dde74f2cd8
danielfirst98 <danielfirst98@gmail.com>
2026-07-31 14:17:04 +0900

    Add input vail error text and style in signup, login
    Add input vail error text and style in signup.js, login.js
    Add test code for event bubling in signup.js

--- script/signup.js
+++ script/signup.js
@@ -1,7 +1,17 @@
-const emailInput = document.querySelector("#login-email");
+const emailInput = document.querySelector("#signup-email");
 const emailErorrComponent = document.querySelector(".email.error");
-const passwordInput = document.querySelector("#login-passwd");
+const passwordInput = document.querySelector("#signup-passwd");
 const passwordErrorComponet = document.querySelector(".passwd.error");
+const passwordInput2 = document.querySelector("#signup-passwd2");
+const passwordErrorComponet2 = document.querySelector(".passwd2.error");
+const totalInput = document.querySelector("form");
+let passwordCompar = "";
+
+//이벤트 위임 테스트 코드(미션 진행중 중단됨)
+console.log(totalInput);
+totalInput.addEventListener("focusin", (event) => {
+  console.log(event.target.closest("input").value);
+});
 
 console.log(passwordInput);
 function vaildateEmail(email) {
@@ -54,5 +64,33 @@ passwordInput.addEventListener("blur", (event) => {
     console.log("비밀번호 정상");
     passwordErrorComponet.style.display = "none";
     passwordInput.style.border = "none";
+  }
+  passwordCompar = value;
+});
+
+passwordInput2.addEventListener("blur", (event) => {
+  const value = event.target.value;
+  console.log(value);
+  if (!value) {
+    console.log("비밀번호 미입력");
+    passwordErrorComponet2.children[0].textContent = "비밀번호를 입력해주세요.";
+    passwordInput2.style.border = "1px solid var(--error-red, #F74747)";
+    passwordErrorComponet2.style.display = "block";
+  } else if (!vaildatePassword(value)) {
+    console.log("비밀번호 짧음");
+    passwordErrorComponet2.children[0].textContent =
+      "비밀번호를 8자리 이상 입력해주세요.";
+    passwordInput2.style.border = "1px solid var(--error-red, #F74747)";
+    passwordErrorComponet2.style.display = "block";
+  } else if (value !== passwordCompar) {
+    console.log("비밀번호 미일치");
+    passwordErrorComponet2.children[0].textContent =
+      "비밀번호가 서로 일치하지 않습니다.";
+    passwordInput2.style.border = "1px solid var(--error-red, #F74747)";
+    passwordErrorComponet2.style.display = "block";
+  } else {
+    console.log("비밀번호 정상");
+    passwordErrorComponet2.style.display = "none";
+    passwordInput2.style.border = "none";
   }
 });
