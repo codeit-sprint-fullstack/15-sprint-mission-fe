99581c5e40313fc045113b5855a5cd1ef4f0785b
danielfirst98 <danielfirst98@gmail.com>
2026-08-23 00:46:57 +0900

    Add product detail page stlye

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useGetPostById } from '../hooks/useGetPostById';
 import { LoadingSpinner } from '../components/LoadingSpinner';
+import styles from '../styles/ProductDetails.module.css';
 
 export function ProductDetails() {
   const { productId } = useParams();
@@ -11,14 +12,24 @@ export function ProductDetails() {
   if (!post) return <LoadingSpinner />;
 
   return (
-    <>
-      <h1>제품상세페이지입니다.</h1>
-      <p>상품명:{post.name}</p>
-      <p>상품소개:{post.description}</p>
-      <p>판매가격:{post.price}</p>
+    <div className={styles.body}>
+      <h1 className={styles.title}>제품상세보기</h1>
+      <img
+        src={post.img}
+        onError={(e) => {
+          e.target.onerror = null;
+          e.target.src = 'comment-empty.png';
+        }}
+        className={styles.imageSize}
+      />
+      <p className={styles.name}>상품명: {post.name}</p>
+      <p className={styles.ment}>상품소개: {post.description}</p>
+      <p className={styles.ment}>판매가격: {post.price}원</p>
       {post.tags.map((tag, index) => (
-        <span key={index}>#{tag} </span>
+        <span key={index} className={styles.ment}>
+          #{tag}{' '}
+        </span>
       ))}
-    </>
+    </div>
   );
 }
